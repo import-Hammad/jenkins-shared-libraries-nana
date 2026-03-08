b045902de847feacb337f93ea6e5c44a39306982
import-Hammad <hammadmalik67000@gmail.com>
2026-03-08 16:29:01 +0500

    Escape variables in docker login command

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -4,7 +4,7 @@ def call (){
     echo  "building  the docker image"
                     withCredentials([usernamePassword(credentialsId:'docker-hub-repo',  passwordVariable: 'PASS', usernameVariable: 'USER')]){
                         sh  'docker build -t piratehammad/demo-app:jma-2.0 .'
-                        sh "echo  $PASS  | docker login -u $USER --password-stdin"
+                        sh "echo  \$PASS  | docker login -u \$USER --password-stdin"
                         sh 'docker push piratehammad/demo-app:jma-2.0'
                     }
 }
