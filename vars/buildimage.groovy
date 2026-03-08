d03436f2934816ca059cde89ee3c667f50aee449
import-hammad <hammadmalik67000@gmail.com>
2026-03-08 17:49:51 +0500

    ok

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,10 +1,6 @@
 #!/usr/bin/env groovy
+import com.example.Docker
 
-def call (){
-    echo  "building  the docker image"
-                    withCredentials([usernamePassword(credentialsId:'docker-hub-repo',  passwordVariable: 'PASS', usernameVariable: 'USER')]){
-                        sh  'docker build -t piratehammad/demo-app:jma-2.0 .'
-                        sh "echo  \$PASS  | docker login -u \$USER --password-stdin"
-                        sh 'docker push piratehammad/demo-app:jma-2.0'
-                    }
+def call() {
+    return new Docker(this).buildDockerImage()
 }
