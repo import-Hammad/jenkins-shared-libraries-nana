b8686f2474e6d98b03aec0492af82d68c4e177cc
import-hammad <hammadmalik67000@gmail.com>
2026-04-12 15:15:40 +0500

    ok

--- vars/buildimage.groovy
+++ vars/buildimage.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
-def call() {
+def call(String imageName) {
     echo "deploying the app"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'docker build -t piratehammad/nana_practice_jenkins_2:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push piratehammad/nana_practice_jenkins_2:jma-2.0'
+        sh "docker push $imageName"
     }
 }
