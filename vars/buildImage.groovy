caf6daa184e5353809948e112a91ff0a173a565d
import-Hammad <hammadmalik67000@gmail.com>
2026-08-02 21:37:07 +0500

    Refactor buildImage function to use Docker class
    Refactored the buildImage function to use a Docker class for building images.

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,15 @@
 #!/usr/bin/env groovy
 
-def call(String imageName) {
-    echo "building the docker image"
-    withCredentials([usernamePassword(credentialsId: 'Dockerhub_credentials', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
-        sh "docker build -t $imageName ."
-        sh 'echo $PASSWORD | docker login -u $USER --password-stdin'
-        sh "docker push $imageName"
-    }
+// def call(String imageName) {
+//     echo "building the docker image"
+//     withCredentials([usernamePassword(credentialsId: 'Dockerhub_credentials', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
+//         sh "docker build -t $imageName ."
+//         sh 'echo $PASSWORD | docker login -u $USER --password-stdin'
+//         sh "docker push $imageName"
+//     }
+// }
+
+import com.example.Docker
+def call (String imageName) {
+    return new Docker(this).buildDockerImage(imageName)
 }
